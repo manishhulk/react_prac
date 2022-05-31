7fda4711b6798fc3635d197a78014c512c90b19f
Manish Banda <manish@hulkapps.com>
2022-05-31 18:35:36 +0530

    working on next.js

--- next_codeswear/components/Navbar.js
+++ next_codeswear/components/Navbar.js
@@ -1,24 +1,28 @@
 import Image from 'next/image'
 import Link from 'next/link'
 import React from 'react'
+import { AiOutlineShoppingCart } from 'react-icons/ai';
 
 const Navbar = () => {
   return (
-    <div className="flex justify-between">
-      <div className="logo">
+    <div className="flex flex-col md:flex-row md:justify-between justify-center items-center">
+      <div className="logo mx-5 ">
         <Link href={"/"}>
           <a>
             <Image src="/logo.png" alt="" width={200} height={40} />
           </a>
         </Link>
       </div>
-      <div className="className">
-        <ul className="flex">
-          <li>Tshirts</li>
-          <li>Hoodies</li>
-          <li>Stickers</li>
-          <li>Mugs</li>
+      <div className="nav">
+        <ul className="flex items-center space-x-2 font-bold">
+          <Link href={"/tshirts"}><a><li>Tshirts</li></a></Link>
+          <Link href={"/"}><a><li>Hoodies</li></a></Link>
+          <Link href={"/"}><a><li>Stickers</li></a></Link>
+          <Link href={"/"}><a><li>Mugs</li></a></Link>
         </ul>
+      </div>
+      <div className="cart">
+        <button><AiOutlineShoppingCart className="text-3xl" /></button>
       </div>
     </div>
   )
